4f0fac29d70ac0d529f7330b5ba264ad58851869
Chris Maikos <chmaikos@gmail.com>
2023-08-21 17:33:28 +0300

    Update readme and switch-to-poetry

---
+++ .github/backup/setup.py
@@ -0,0 +1,46 @@
+"""Python setup.py for image_quality_sampler package"""
+import io
+import os
+from setuptools import find_packages, setup
+
+
+def read(*paths, **kwargs):
+    """Read the contents of a text file safely.
+    >>> read("image_quality_sampler", "VERSION")
+    '0.1.0'
+    >>> read("README.md")
+    ...
+    """
+
+    content = ""
+    with io.open(
+        os.path.join(os.path.dirname(__file__), *paths),
+        encoding=kwargs.get("encoding", "utf8"),
+    ) as open_file:
+        content = open_file.read().strip()
+    return content
+
+
+def read_requirements(path):
+    return [
+        line.strip()
+        for line in read(path).split("\n")
+        if not line.startswith(('"', "#", "-", "git+"))
+    ]
+
+
+setup(
+    name="image_quality_sampler",
+    version=read("image_quality_sampler", "VERSION"),
+    description="Awesome image_quality_sampler created by Red-Net-Internet-Services",
+    url="https://github.com/Red-Net-Internet-Services/image_quality_sampler/",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    author="Red-Net-Internet-Services",
+    packages=find_packages(exclude=["tests", ".github"]),
+    install_requires=read_requirements("requirements.txt"),
+    entry_points={
+        "console_scripts": ["image_quality_sampler = image_quality_sampler.__main__:main"]
+    },
+    extras_require={"test": read_requirements("requirements-test.txt")},
+)
